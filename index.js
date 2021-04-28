59ecdece4b03d83291c2e22494aee9ad4bdbb176
BailiMohamedAli <bailimohamedali@gmail.com>
2021-04-28 15:11:06 +0100

    decryption setup and working -->next reconfigure to run on a server and frot end to store encryted data in DB

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const encrypt = require('./encryption/encrypt');
-
+const decrypt = require('./encryption/decrypt');
+//keys
 const publicKey = fs.readFileSync(__dirname + '/keys/id_rsa_pub.pem', 'utf-8');
+const privateKey = fs.readFileSync(__dirname + '/keys/id_rsa_priv.pem', 'utf-8');
 //store in buffer
-const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, 'message to encrypt in here')
+const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, 'message to encrypt in here');
 //checking result by Logs
 console.log(encryptedMessage.toString());
+//decrypting data;
+const decryptedMessage = decrypt.decryptWithPrivateKey(privateKey, encryptedMessage);
+//checking result by
+console.log(decryptedMessage.toString());
