89dbccb8ff56c77f2aa4ae0781d9d68301abe98b
BailiMohamedAli <bailimohamedali@gmail.com>
2021-04-30 14:55:09 +0100

    seting up the frontend add data form --> preparing the submit to send back en encrypt and save data to db and seting up db

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 const fs = require('fs');
 const encrypt = require('./encryption/encrypt');
 const decrypt = require('./encryption/decrypt');
 //keys
 const publicKey = fs.readFileSync(__dirname + '/keys/id_rsa_pub.pem', 'utf-8');
 const privateKey = fs.readFileSync(__dirname + '/keys/id_rsa_priv.pem', 'utf-8');
-//store in buffer
-const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, 'message to encrypt in here');
-//checking result by Logs
-console.log(encryptedMessage.toString());
-//decrypting data;
-const decryptedMessage = decrypt.decryptWithPrivateKey(privateKey, encryptedMessage);
-//checking result by
-console.log(decryptedMessage.toString());
+const PORT = 3003;
+
+//setup view engine, assets dir and bassic json protocol
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routing data
+const nav ={
+    local: '',
+    username: ''
+}
+
+app.get('/', (req, res) => {
+    nav.local= 'home';
+    res.render('pages/home', {nav : nav});
+})
+app.get('/newdata', (req, res) => {
+    nav.local= 'addData';
+    res.render('pages/adddata', {nav : nav});
+})
+//listning to server
+app.listen(PORT, () => console.log(`server runnig on port ${PORT}:\nhttp://localhost:${PORT}`));
